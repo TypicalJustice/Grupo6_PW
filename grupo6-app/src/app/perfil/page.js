07ea5e43fc981f90b52e25a01f9a0773904c24f4
Orlando Aguilar <orlando.aguilar1702@gmail.com>
2023-05-15 14:21:21 -0500

    cambiosperfil

--- grupo6-app/src/app/perfil/page.js
+++ grupo6-app/src/app/perfil/page.js
@@ -32,34 +32,62 @@ const Perfil = () => {
                         <button>Guardar</button>
                     </div>
                     <h4>Información Personal</h4>
-                    <Form>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Nombres</Form.Label>
-                            <Form.Control required type="text" placeholder='Nombres' />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Apellidos</Form.Label>
-                            <Form.Control required type="text" placeholder='Apellidos' />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Tipo de documento</Form.Label>
-                            <Form.Select aria-label="Default select example">
-                                <option></option>
-                                <option value="1">DNI</option>
-                                <option value="2">Carné de extranjeria</option>
-                            </Form.Select>
-                        </Form.Group>
-                    </Form>
-                    <Form>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Rol</Form.Label>
-                            <Form.Control required type="text" placeholder='Ingrese su rol'/>
-                        </Form.Group>
-                        <Form.Group className="mb-34">
-                            <Form.Label>Número de documento</Form.Label>
-                            <Form.Control required type="text" placeholder='Ingrese número' />
-                        </Form.Group>
-                    </Form>
+                    <div className='formularioarriba'>
+                        <Form>
+                            <Form.Group className="mb-3arriba">
+                                <Form.Label>Nombres</Form.Label>
+                                <Form.Control required type="text" placeholder='Nombres' className='arriba'/>
+                            </Form.Group>
+                            <Form.Group className="mb-3arriba">
+                                <Form.Label>Apellidos</Form.Label>
+                                <Form.Control required type="text" placeholder='Apellidos' className='arriba'/>
+                            </Form.Group>
+                            <Form.Group className="mb-3arriba">
+                                <Form.Label>Tipo de documento</Form.Label>
+                                <Form.Select aria-label="Default select example" className='arriba'>
+                                    <option value="1">DNI</option>
+                                    <option value="2">Carné de extranjeria</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Form>
+                        <Form>
+                            <Form.Group className="mb-3arriba">
+                                <Form.Label>Rol</Form.Label>
+                                <Form.Control required type="text" placeholder='Ingrese su rol' className='arriba'/>
+                            </Form.Group>
+                            <Form.Group className="mb-34">
+                                <Form.Label>Número de documento</Form.Label>
+                                <Form.Control required type="text" placeholder='Ingrese número' className='arriba'/>
+                            </Form.Group>
+                        </Form>
+                    </div>
+                    <div className='encabezadoinferior'>
+                        <p>Datos de Usuario</p>
+                        <p>Universidad</p>
+                        <p>Presentación</p>
+                    </div>
+                    <div className='formularioabajo'>
+                        <Form>
+                            <Form.Group className="mb-3abajo">
+                                <Form.Label>Usuario</Form.Label>
+                                <Form.Control required type="text" placeholder='Nombre del usuario' className='abajo'/>
+                            </Form.Group>
+                        </Form>
+                        <Form>
+                            <Form.Group className="mb-3abajo">
+                                <Form.Label>Contraseña Actual</Form.Label>
+                                <Form.Control required type="password" placeholder='Ingrese su contraseña' className='abajo'/>  
+                            </Form.Group>
+                            <Form.Group className="mb-3abajo">
+                                <Form.Label>Nueva Contraseña</Form.Label>
+                                <Form.Control required type="password" placeholder='Ingrese nueva contraseña' className='abajo'/>  
+                            </Form.Group>
+                            <Form.Group className="mb-3abajo">
+                                <Form.Label>Repetir Contraseña</Form.Label>
+                                <Form.Control required type="password" placeholder='Repita nueva contraseña' className='abajo'/>
+                            </Form.Group>
+                        </Form>
+                    </div>
                 </div>
             </main>
         </div>
