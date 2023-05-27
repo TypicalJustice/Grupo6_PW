7d4db0c56fce40f81602454a0d8341b093f4a3b2
aadrian00 <aaai2007@hotmail.com>
2023-05-27 02:53:39 -0500

    Update page.js

--- grupo6-app/src/app/citas/page.js
+++ grupo6-app/src/app/citas/page.js
@@ -8,20 +8,27 @@ const Citas = () => {
     { id: 3, date: '2023-07-01', time: '11:30 AM', estudiante: 'Kevin de Bruyne' },
   ];
 
-  const userType = 'estudiante'; // El usuario por default es estudiante, se debe cambiar a 'docente' para el perfil del docente.
+  const citasPasadas = [
+    { id: 4, date: '2023-05-15', time: '3:00 PM', estudiante: 'Neymar Silva' },
+    { id: 5, date: '2023-05-20', time: '9:30 AM', estudiante: 'Andrés Iniesta' },
+  ];
+
+  const userType = 'estudiante'; // El usuario por defecto es estudiante, se debe cambiar a 'docente' para el perfil del docente.
 
   const perfilEstudiante = () => {
     return (
       <div>
         <h2>Próximas citas</h2>
         <ul>
-          {proximasCitas.map((citas) => (
-            <li key={citas.id}>
-              <strong>Fecha:</strong> {citas.date} | <strong>Hora:</strong> {citas.time} | <strong>Estudiante:</strong> {citas.estudiante}
+          {proximasCitas.map((cita) => (
+            <li key={cita.id}>
+              <strong>Fecha:</strong> {cita.date} | <strong>Hora:</strong> {cita.time} | <strong>Estudiante:</strong> {cita.estudiante}
             </li>
           ))}
         </ul>
         <a href="/nueva-cita">Sacar una nueva cita</a>
+        <br />
+        <a href="/citas-pasadas">Ver citas pasadas</a>
       </div>
     );
   };
@@ -40,13 +47,44 @@ const Citas = () => {
         <a href="/administrar-horarios">Administrar horarios</a>
         <br />
         <a href="/ver-calificaciones">Ver calificaciones</a>
+        <br />
+        <a href="/citas-pasadas">Ver citas pasadas</a>
       </div>
     );
   };
 
+  const detalleCita = (cita) => {
+    const calificarAsesoria = () => {
+      // Lógica para calificar la asesoría
+      // ...
+      alert('¡Gracias por calificar la asesoría!');
+    };
+
+    return (
+      <div>
+        <h2>Detalle de la cita</h2>
+        <p>
+          <strong>Fecha:</strong> {cita.date} | <strong>Hora:</strong> {cita.time} | <strong>Estudiante:</strong> {cita.estudiante}
+        </p>
+        {userType === 'estudiante' && (
+          <button onClick={calificarAsesoria}>Calificar asesoría</button>
+        )}
+      </div>
+    );
+  };
+
+  // Obtener el parámetro de la URL para el detalle de la cita
+  const urlParams = new URLSearchParams(window.location.search);
+  const citaId = urlParams.get('citaId');
+  const citaSeleccionada = proximasCitas.find((cita) => cita.id.toString() === citaId);
+
   return (
     <div>
-      {userType === 'estudiante' ? perfilEstudiante() : perfilDocente()}
+      {citaSeleccionada ? (
+        detalleCita(citaSeleccionada)
+      ) : (
+        userType === 'estudiante' ? perfilEstudiante() : perfilDocente()
+      )}
     </div>
   );
 };
